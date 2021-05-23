9b3593e5bfc2b70d702cb444db2c60cb58e0a805
Darogon01 <78644182+Darogon01@users.noreply.github.com>
2021-05-23 19:51:01 +0200

    Add getCuestion and localStorage

--- script/script.js
+++ script/script.js
@@ -1,36 +1,54 @@
-/* let quiz = window.location.herf = "../script/.question.json" */
-/* console.log(quiz) */
-async function getQuestionsAsync(questions) 
-{
+async function getQuestionsAsync(questions) {
   let response = await fetch(`${questions}`);
-  let data = await response.json()
+  let data = await response.json();
   return data;
-  
 }
+let newData = [{fecha:'13/12/2020',aciertos:0},{fecha:'14/12/2020',aciertos:6},{fecha:'15/12/2020',aciertos:1}]
+getQuestionsAsync("../script/question.json")
+  .then((data) => {
+    /*  console.log(data);
+    console.log(randElemnt(data)); */
+    /* addItem(data); */
+    sumaData()
+  })
 
-getQuestionsAsync("../question.json")
-  .then(data => console.log(data))
-  .catch(error => console.log("hubo un error"+error));
+  .catch((error) => console.log("hubo un error" + error));
 
-/* localStorage.setItem(results, JSON.stringify({
-    
-})); */
-//Leer
-let resultLocalStore = JSON.parse(localStorage.getItem('user'));
+let randElemnt = (array) => {
+  let result = [];
+  while (result.length < array.length) {
+    let random = Math.floor(Math.random() * array.length);
+    let element = array[random];
+    if (result.indexOf(element) == -1) result.push(element);
+  }
+  return result;
+};
+/* guardar y sumar datos localStorage */
+let resultLocalStore = JSON.parse(localStorage.getItem("results"));
+let addItem = (array) => {
+  localStorage.setItem("resultados", JSON.stringify(array));
+};
+let getItem = (key) => {
+  let resultLocalStore = JSON.parse(localStorage.getItem(key));
+  return resultLocalStore;
+};
+let sumaData = () => {
+  let oldData = getItem("resultados");
+  let suma = oldData.concat(newData);
+  addItem(suma)
+};
 
 const result1 = document.getElementById("number1");
 const result2 = document.getElementById("number2");
-console.log()
+console.log();
 
-function paintResults(data1,data2){
-    
-    result1.innerHTML = data1
-    
-    result2.innerHTML = data2
+function paintResults(data1, data2) {
+  result1.innerHTML = data1;
 
+  result2.innerHTML = data2;
 }
-paintResults("2","5")
+paintResults("2", "5");
 
-document.getElementById("boton").addEventListener("click",(e)=>{
-    window.location.href="../index.html"
+document.getElementById("boton").addEventListener("click", (e) => {
+  window.location.href = "../index.html";
 });
