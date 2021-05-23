d8d1e562fb576fb7dc942437984ed0ee3c8c0695
Darogon01 <78644182+Darogon01@users.noreply.github.com>
2021-05-23 13:19:52 +0200

    Add question .JSON

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
+/* let quiz = window.location.herf = "../script/.question.json" */
+/* console.log(quiz) */
+async function getQuestionsAsync(questions) 
+{
+  let response = await fetch(`${questions}`);
+  let data = await response.json()
+  return data;
+  
+}
+
+getQuestionsAsync("../question.json")
+  .then(data => console.log(data))
+  .catch(error => console.log("hubo un error"+error));
+
+/* localStorage.setItem(results, JSON.stringify({
+    
+})); */
+//Leer
+let resultLocalStore = JSON.parse(localStorage.getItem('user'));
+
 const result1 = document.getElementById("number1");
 const result2 = document.getElementById("number2");
 console.log()
@@ -9,8 +29,8 @@ function paintResults(data1,data2){
     result2.innerHTML = data2
 
 }
-paintResults("1","5")
+paintResults("2","5")
 
 document.getElementById("boton").addEventListener("click",(e)=>{
-    window.location.href="index.html"
+    window.location.href="../index.html"
 });
